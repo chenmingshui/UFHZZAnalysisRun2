c0b55b0636f3260c7c80d933c9a4506ecec24f1b
Mingshui Chen <chenmingshui@gmail.com>
2018-01-24 14:00:43 +0100

    for 94X

--- install.sh
+++ install.sh
@@ -12,19 +12,23 @@ git clone https://github.com/VBF-HZZ/KinZfitter.git
 
 # this is still 2016 calibration from backtis, may change later
 git clone https://github.com/bachtis/Analysis.git -b KaMuCa_V4 KaMuCa
-git clone https://github.com/cms-analysis/HiggsAnalysis-ZZMatrixElement.git ZZMatrixElement
+git clone https://github.com/chenmingshui/HiggsAnalysis-ZZMatrixElement.git -b from-v211 ZZMatrixElement
 
 #the following patch has been manually handled
 #cp UFHZZAnalysisRun2/Utilities/fixcrab.patch ZZMatrixElement
 
 
 #git cms-merge-topic -u perrozzi:HTXS_clean
- git remote add perrozzi_HTXS_clean  https://github.com/perrozzi/cmssw.git
- git fetch perrozzi_HTXS_clean
- git branch HTXS_clean perrozzi_HTXS_clean/HTXS_clean
+ #git remote add perrozzi_HTXS_clean  https://github.com/perrozzi/cmssw.git
+ #git fetch perrozzi_HTXS_clean
+ #git branch HTXS_clean perrozzi_HTXS_clean/HTXS_clean
+ git remote add chen_HTXS_clean https://github.com/chenmingshui/cmssw.git
+ git fetch chen_HTXS_clean 
+ git branch HTXS_clean chen_HTXS_clean/HTXS_clean
  git checkout HTXS_clean SimDataFormats/HTXS
  git checkout HTXS_clean GeneratorInterface/RivetInterface
 
-(cd ZZMatrixElement ; git checkout -b from-v211 v2.1.1 ; ./setup.sh -j 12)
+
+(cd ZZMatrixElement ; ./setup.sh -j 12)
 
 scram b -j 8
